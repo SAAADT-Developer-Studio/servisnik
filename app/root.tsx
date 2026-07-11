913c16379935c8591d476447b65de5a2b67b61e3
marko154 <marko.gartnar777@gmail.com>
2026-07-11 20:09:32 +0200

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,21 @@ import { getOwnerPendingTicketCount } from "./tickets/tickets.server";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
+	{ rel: "icon", href: "/favicon.ico", sizes: "any" },
+	{
+		rel: "icon",
+		type: "image/png",
+		sizes: "32x32",
+		href: "/favicon-32x32.png",
+	},
+	{
+		rel: "icon",
+		type: "image/png",
+		sizes: "16x16",
+		href: "/favicon-16x16.png",
+	},
+	{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+	{ rel: "manifest", href: "/site.webmanifest" },
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
 	{
 		rel: "preconnect",
